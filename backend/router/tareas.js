47b5226caac81484556624bfb255b6aaf77cff4b
Franco Vaccari <francovacc2.0@gmail.com>
2023-06-01 19:12:24 -0300

    backend POST

--- backend/router/tareas.js
+++ backend/router/tareas.js
@@ -17,32 +17,14 @@ const sql = 'SELECT ID_tareas, Nombre_tareas, Descripcion_tareas,(SELECT Nombre_
 routerTareas.get('/', (req, res) => {
     try {
         db.all(sql, [], (err, rows) => {
-            if (err) {
-                return res.status(300).json({
-                    status: 300,
-                    error: err,
-                    success: false
-                })
-            }
-            if (rows.length === 0) {
-                return res.status(404).json({
-                    status: 404,
-                    error: "NO DATA FOUNDED",
-                    success: false
-                })
-            }
-            return res.json({
-                stuatus: 200,
-                tareas: rows,
-                success: true
-            })
+            if (err) return res.status(300).json({ status: 300, error: err, success: false })
+
+            if (rows.length === 0) return res.status(404).json({ status: 404, error: "NO DATA FOUNDED", success: false })
+
+            return res.json({ stuatus: 200, tareas: rows, success: true })
         })
     } catch (error) {
-        res.status(500).json({
-            status: 500,
-            error,
-            success: false
-        })
+        res.status(500).json({ status: 500, error, success: false })
     }
 })
 
@@ -50,44 +32,18 @@ routerTareas.get('/', (req, res) => {
 routerTareas.get('/pendientes/:pendientes', (req, res) => {
     const { pendientes } = req.params;
     try {
-        if (pendientes < '0' || pendientes > '1') {
+        if (pendientes < '0' || pendientes > '1') return res.status(404).json({ status: 404, error: "NO DATA FOUNDED", success: false })
 
-            return res.status(404).json({
-                status: 404,
-                error: "NO DATA FOUNDED",
-                success: false
-            })
-        }
         let query = sql + ' WHERE Pendiente_tareas = ?';
         db.all(query, [pendientes], (err, rows) => {
-            if (err) {
-                return res.status(300).json({
-                    status: 300,
-                    error: err,
-                    success: false
-                })
-            }
-            if (rows.length == 0) {
-                return res.status(404).json({
-                    status: 404,
-                    error: "NO DATA FOUNDED",
-                    success: false
-                })
-            }
+            if (err) return res.status(300).json({ status: 300, error: err, success: false })
 
-            return res.json({
-                status: 200,
-                data: rows,
-                success: true
-            })
+            if (rows.length == 0) return res.status(404).json({ status: 404, error: "NO DATA FOUNDED", success: false })
+
+            return res.json({ status: 200, data: rows, success: true })
         })
     } catch (error) {
-
-        res.status(500).json({
-            status: 500,
-            error,
-            success: false
-        })
+        res.status(500).json({ status: 500, error, success: false })
     }
 })
 
@@ -99,36 +55,39 @@ routerTareas.get('/importancia/:importancia', (req, res) => {
         if (importancia === "muy importante" || importancia === "regular" || importancia === "no importante") {
             const query = sql + ' WHERE importancia = ?';
             db.all(query, [importancia], (err, rows) => {
-                if (err) {
-                    return res.status(300).json({
-                        status: 300,
-                        error: err,
-                        success: false
-                    })
-                }
-                if (rows.length == 0) {
-                    return res.status(404).json({
-                        status: 404,
-                        error: "NO DATA FOUNDED",
-                        success: false
-                    })
-                }
+                if (err) return res.status(300).json({ status: 300, error: err, success: false })
+                if (rows.length == 0) return res.status(404).json({ status: 404, error: "NO DATA FOUNDED", success: false })
 
-                return res.json({
-                    status: 200,
-                    data: rows,
-                    success: true
-                })
+                return res.json({ status: 200, data: rows, success: true })
             })
         }
 
 
     } catch (error) {
-        res.status(500).json({
-            status: 500,
-            error,
-            success: false
+        res.status(500).json({ status: 500, error, success: false })
+    }
+})
+
+//? POST - nueva tarea
+routerTareas.post('/', (req, res) => {
+    const { nombre, descripcion, importancia, pendiente } = req.body;
+    console.log(req.body)
+    const query = 'INSERT INTO tareas (Nombre_tareas, Descripcion_tareas, ID_importancia, Pendiente_tareas) VALUES (?,?,?,?)'
+    try {
+        if (!req.body) return res.status(500).json({ status: 500, error: "NO DATA TO LOAD", success: false })
+
+        if (nombre === " " || !nombre || importancia < 1 || importancia > 3 || !importancia || descripcion === " " || !descripcion || pendiente < 0 || pendiente > 1 || !pendiente) return res.status(500).json({ status: 500, error: "SOME INFORMATION IS MISSING OR INCORRECT", success: false })
+
+        db.run(query, [nombre, descripcion, importancia, pendiente], (err) => {
+            if (err) return res.status(500).json({ status: 500, error: err, success: false });
+
+            return res.json({ status: 200, message: "DATA ACEPTED", success: true })
+
         })
+
+
+    } catch (error) {
+        res.status(500).json({ status: 500, error, success: false })
     }
 })
 module.exports = routerTareas
