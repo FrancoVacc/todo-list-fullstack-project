8029c6cf8dcd630204aab614602a7c343a46e133
Franco Vaccari <francovacc2.0@gmail.com>
2023-06-02 12:04:15 -0300

    backend put & delete

--- backend/router/tareas.js
+++ backend/router/tareas.js
@@ -20,7 +20,6 @@ routerTareas.get('/', (req, res) => {
             if (err) return res.status(300).json({ status: 300, error: err, success: false })
 
             if (rows.length === 0) return res.status(404).json({ status: 404, error: "NO DATA FOUNDED", success: false })
-
             return res.json({ stuatus: 200, tareas: rows, success: true })
         })
     } catch (error) {
@@ -60,8 +59,9 @@ routerTareas.get('/importancia/:importancia', (req, res) => {
 
                 return res.json({ status: 200, data: rows, success: true })
             })
+            res.status(404).json({ status: 404, error: "NO DATA FOUNDED", success: false })
         }
-
+        res.status(404).json({ status: 404, error: "DATA NOT FOUND", success: false })
 
     } catch (error) {
         res.status(500).json({ status: 500, error, success: false })
@@ -90,4 +90,42 @@ routerTareas.post('/', (req, res) => {
         res.status(500).json({ status: 500, error, success: false })
     }
 })
+
+
+//? UPDATE - PUT
+routerTareas.put('/update/:id', (req, res) => {
+    const { nombre, descripcion, importancia, pendiente } = req.body;
+    const id = req.params.id
+    console.log(nombre, descripcion, importancia, pendiente)
+    try {
+        const query = 'UPDATE tareas SET Nombre_tareas = ?, Descripcion_tareas = ?, ID_importancia = ?, Pendiente_tareas = ? WHERE ID_tareas = ?';
+        db.run(query, [nombre, descripcion, importancia, pendiente, id], (err) => {
+            if (err) {
+                console.log(err)
+                return res.status(500).json({ status: 500, error: err, success: false })
+            };
+            return res.json({ status: 200, message: "DATA UPDATED", success: true })
+        })
+    } catch (error) {
+        res.status(500).json({ status: 500, error, success: false })
+    }
+})
+
+//? DELETE
+routerTareas.delete('/delete/:id', (req, res) => {
+    const id = req.params.id
+    try {
+        const query = 'DELETE FROM tareas WHERE ID_tareas = ?';
+        db.run(query, [id], (err) => {
+            if (err) {
+                console.log(err)
+                return res.status(500).json({ status: 500, error: err, success: false })
+            };
+            return res.json({ status: 200, message: "DATA DELETED", success: true })
+        })
+    } catch (error) {
+        res.status(500).json({ status: 500, error, success: false })
+    }
+})
+
 module.exports = routerTareas
